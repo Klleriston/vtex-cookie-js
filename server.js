6d4ba9c4e63937473e934f85a46fb2c493aaaea3
klleriston <klleandrade@outlook.com>
2024-11-08 14:01:46 -0300

    feat: add route for chatbot

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import fetch from 'node-fetch';
 const app = express();
 app.use(express.json());
 
-const cookie = 'eyJhbGciOiJFUzI1NiIsImtpZCI6ImNkNTk0Mzg2LTAyYzUtNGE2Mi1hMzI4LWI3ZTM3YTg0MjFiOCIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50LmlkIjpbXSwiaWQiOiI0MWQwODQ0YS03ZDJiLTRiNTMtOTUzOC1mY2U2NDU2ZjBhNTAiLCJ2ZXJzaW9uIjo0LCJzdWIiOiJzZXNzaW9uIiwiYWNjb3VudCI6InNlc3Npb24iLCJleHAiOjE3MzE2NzAyMDcsImlhdCI6MTczMDk3OTAwNywianRpIjoiZmVkZDI5NmItMTAyOC00YTFkLTk3NTYtZDUyYjc1NzgyM2Y4IiwiaXNzIjoic2Vzc2lvbi9kYXRhLXNpZ25lciJ9.Reuf3g8iEyMeptPmaoGKzaZyerHCaZkPmNjJ8b2KqrjwkPUqraR6mOVDEgdEsDxfUD_sQW5xb941SjFSmfe1dQ;';
-
+const cookie = 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ijc0Zjk1YzZhLTY3NmYtNGNmMi05MmI3LWJhOGM5NThlYmJiNSIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50LmlkIjpbXSwiaWQiOiJhZWJjYjI0MC00NTk4LTQ3YWUtYTViNC04YmNkZTNmOWEzZjEiLCJ2ZXJzaW9uIjo5LCJzdWIiOiJzZXNzaW9uIiwiYWNjb3VudCI6InNlc3Npb24iLCJleHAiOjE3MzE3NjMxMTgsImlhdCI6MTczMTA3MTkxOCwianRpIjoiNTEzODQyYTYtNjFlMS00ZTZmLTkxMmItZjQ3NTkxNTYwYzBjIiwiaXNzIjoic2Vzc2lvbi9kYXRhLXNpZ25lciJ9.ld4BKSMT0eDscNCCrpDcNGiFB3ClKoBRLyLk5vSU_bcA2Hni4IXGVwDKiOMoPCzyb_i5vHMr5iOW_RQy_67Ksw';
 async function fetchSessionData() {
   const response = await fetch('https://www.futuro.digital/api/sessions?items=*', {
     headers: {
@@ -14,8 +13,41 @@ async function fetchSessionData() {
     },
   });
   const data = await response.json();
-  return data.namespaces.public.stateuf;
+  return data;
 }
+
+async function fetchBotTalk(userMessage) {
+  const res = await fetch('https://api.futuro-digital.dev.senai.br/chatbot/', { 
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer foobar',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "messages": [
+        {
+          "role": "user",
+          "content": userMessage
+        }
+      ]
+    })
+  });
+  const data = await res.json();
+  return data; 
+}
+
+app.post('/api/bot-talk', async (req, res) => {
+  try {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const { message } = req.body;
+    const botResponse = await fetchBotTalk(message); 
+    console.log(botResponse);
+    res.json({ botResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao conectar com a API');
+  }
+});
 
 
 app.get('/api/sessions', async (req, res) => {
@@ -40,4 +72,4 @@ app.post('/api/sessions-data', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Proxy rodando na porta 3000'));
+app.listen(3000, () => console.log('rodando na porta 3000'));
